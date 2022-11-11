9b5e166989c51d5c228053fe0ccee8ebc988630b
succ <>
2022-11-11 15:13:44 +0800

    text

--- src/main/java/com/Dao/Main.java
+++ src/main/java/com/Dao/Main.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 import java.util.List;
 import java.util.Map;
-
+//
 @ComponentScan("com")
 public class Main {
     public static void main(String[] args) {
