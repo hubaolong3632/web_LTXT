3b9f1a115c785c00914de3b9b742516b513bd49e
your_username <911412667@qq.com>
2022-11-11 13:18:31 +0800

    form层添加

--- src/main/java/com/Dao/Main.java
+++ src/main/java/com/Dao/Main.java
@@ -24,9 +24,9 @@ public class Main {
             System.out.println(from.getName()+"  ---- "+from.getPassword());
         }
 //
-         int num = login.addInfo(new Info("18762893132", "3142436228@qq.com",
-                "\"C:\\Users\\lxy\\Desktop\\网页\\作业\\素材\\农业网站版本四\\0首页.jpg\""
-                ));
+//         int num = login.addInfo(new Info("18762893132", "3142436228@qq.com",
+//                "\"C:\\Users\\lxy\\Desktop\\网页\\作业\\素材\\农业网站版本四\\0首页.jpg\""
+//                ));
         if(num>0){
             System.out.println("添加信息成功");
         }else{
