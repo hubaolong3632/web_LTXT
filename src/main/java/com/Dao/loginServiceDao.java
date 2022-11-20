c62171e08bdd01e054cfec1489ccacc9b39279ec
your_username <911412667@qq.com>
2022-11-20 10:36:03 +0800

    上传文件2

--- src/main/java/com/Dao/loginServiceDao.java
+++ src/main/java/com/Dao/loginServiceDao.java
@@ -21,19 +21,18 @@ public class loginServiceDao implements IServiceDao {
     JdbcTemplate jdbc_link; //注入
 
 
-
     //判断账号密码的登录
     @Override
-    public LoginModel user_pwd(LoginModel pas){
+    public LoginModel user_pwd(LoginModel pas) {
         System.out.println("006: dao-判断账号是否登入成功>");
 //        System.out.println(pas.getName()+"    "+pas.getPassword());
-        try{
-            String sql2="SELECT * FROM `t_login` where name =? and password=?;";
-            RowMapper<LoginModel> pasword=new BeanPropertyRowMapper(LoginModel.class); //获取Pasowrd类
-            LoginModel  query2 = jdbc_link.queryForObject(sql2, pasword,pas.getName(),pas.getPassword()); //查询返回对象
+        try {
+            String sql2 = "SELECT * FROM `t_login` where name =? and password=?;";
+            RowMapper<LoginModel> pasword = new BeanPropertyRowMapper(LoginModel.class); //获取Pasowrd类
+            LoginModel query2 = jdbc_link.queryForObject(sql2, pasword, pas.getName(), pas.getPassword()); //查询返回对象
 
             return query2;
-        }catch (Exception e){ //否则返回的是spring数据库连接错误
+        } catch (Exception e) { //否则返回的是spring数据库连接错误
             System.out.println("当前用户进行了登入但是 账号密码错误了！");
 //            e.printStackTrace();
             return null;
@@ -41,15 +40,15 @@ public class loginServiceDao implements IServiceDao {
     }
 
     @Override
-    public LoginModel user_zc(LoginModel pas){ //判断是否有当前姓名了
-        System.out.println("输出名称："+pas.getName());
-        try{
-            String sql2="SELECT * FROM `t_login` where name =? ;";
-            RowMapper<LoginModel> pasword=new BeanPropertyRowMapper(LoginModel.class); //获取Pasowrd类
-            LoginModel  query2 = jdbc_link.queryForObject(sql2, pasword,pas.getName()); //查询返回对象
+    public LoginModel user_zc(LoginModel pas) { //判断是否有当前姓名了
+        System.out.println("输出名称：" + pas.getName());
+        try {
+            String sql2 = "SELECT * FROM `t_login` where name =? ;";
+            RowMapper<LoginModel> pasword = new BeanPropertyRowMapper(LoginModel.class); //获取Pasowrd类
+            LoginModel query2 = jdbc_link.queryForObject(sql2, pasword, pas.getName()); //查询返回对象
 
             return query2;
-        }catch (Exception e){ //否则返回的是spring数据库连接错误
+        } catch (Exception e) { //否则返回的是spring数据库连接错误
 //            e.printStackTrace();
             System.out.println("002 :  查询到当前数据库没有当前用户 进行用户的保存");
             return null;
@@ -57,34 +56,33 @@ public class loginServiceDao implements IServiceDao {
     }
 
 
-
     //添加用户信息
     @Override
     public boolean addInfo(LoginModel login) {
         System.out.println("-----查询------");
-         //传入 手机号 邮箱
-        try{
+        //传入 手机号 邮箱
+        try {
 
 //            LoginModel loginModel = user_zc(login); //查询账号密码是否存在  如果抛出了异常那么代表 1.没查询到  2.添加相同的主键了！
 //            if(loginModel==null){ //如果不存在当前账号
-                InfoModel info = login.getInfo(); // 获取个人信息
-                String sqlLogin=" insert into t_login (name,password) VALUES (?,?);"; //插入账号密码的
-                String sqlInfo=" insert into `t_info` (phone,email,headimg,fins,uname) VALUES (?,?,?,?,?);";
-                System.out.println(info.getPhone()+"   "+info.getEmail()+"   "+info.getHeadimg()+"   "+login.getName()+"   "+login.getPassword());
+            InfoModel info = login.getInfo(); // 获取个人信息
+            String sqlLogin = " insert into t_login (name,password) VALUES (?,?);"; //插入账号密码的
+            String sqlInfo = " insert into `t_info` (phone,email,headimg,fins,uname) VALUES (?,?,?,?,?);";
+            System.out.println(info.getPhone() + "   " + info.getEmail() + "   " + info.getHeadimg() + "   " + login.getName() + "   " + login.getPassword());
 
-                int num = jdbc_link.update(sqlLogin,login.getName(),login.getPassword()); //添加账号密码
-                int num1 = jdbc_link.update(sqlInfo,info.getPhone(),info.getEmail(), info.getHeadimg(),0,login.getName()); //添加个人信息
-                System.out.println(num1);
-                if(num1!=0){ //如果注册成功
-                    System.out.println("注册成功");
-                    return true;
-                }
+            int num = jdbc_link.update(sqlLogin, login.getName(), login.getPassword()); //添加账号密码
+            int num1 = jdbc_link.update(sqlInfo, info.getPhone(), info.getEmail(), info.getHeadimg(), 0, login.getName()); //添加个人信息
+            System.out.println(num1);
+            if (num1 != 0) { //如果注册成功
+                System.out.println("注册成功");
+                return true;
+            }
 
 
 //            }
             System.out.println("注册失败:由于姓名已经存在");
             return false;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace(); //500错误好看一点
             System.out.println("注册失败2：未知错误");
             return false;
@@ -102,7 +100,7 @@ public class loginServiceDao implements IServiceDao {
                 login.getName(),
                 login.getPassword()
         };
-        int num = jdbc_link.update(sqlLogin,objLogin);
+        int num = jdbc_link.update(sqlLogin, objLogin);
         return num;
     }
 
@@ -111,9 +109,9 @@ public class loginServiceDao implements IServiceDao {
         String sql = "select * from t_goodfriend where uname=?";
 
 
-        List<GoodFriendModel> query = jdbc_link.query(sql, new BeanPropertyRowMapper<>(GoodFriendModel.class),login.getName());
+        List<GoodFriendModel> query = jdbc_link.query(sql, new BeanPropertyRowMapper<>(GoodFriendModel.class), login.getName());
         for (GoodFriendModel model : query) {
-            System.out.println("姓名： "+model.getFname()+" 好友id：   "+model.getId());
+            System.out.println("姓名： " + model.getFname() + " 好友id：   " + model.getId());
         }
 
         return query;
@@ -124,7 +122,7 @@ public class loginServiceDao implements IServiceDao {
 //        System.out.println(classlfy.getName());
         String sql = "select count(classify) as 'classify'  from t_myarticle where classify=?;";
         BeanPropertyRowMapper<ClassLfyModel> classflyBean = new BeanPropertyRowMapper<>(ClassLfyModel.class);
-        classlfy = jdbc_link.queryForObject(sql, classflyBean,classlfy.getName());
+        classlfy = jdbc_link.queryForObject(sql, classflyBean, classlfy.getName());
 //        System.out.println(classlfy.getClassify());
         return classlfy;
     }
@@ -136,23 +134,23 @@ public class loginServiceDao implements IServiceDao {
         System.out.println(sql);
         System.out.println(model.getClassify().getName());
         BeanPropertyRowMapper<MyarticleModel> myarticlemodel = new BeanPropertyRowMapper<>(MyarticleModel.class);
-        List<MyarticleModel> list= jdbc_link.query(sql,myarticlemodel,model.getClassify().getName());
+        List<MyarticleModel> list = jdbc_link.query(sql, myarticlemodel, model.getClassify().getName());
 
         return list;
     }
 
     @Override
     //添加文章方法
-    public boolean addMyarticle(MyarticleModel model){
+    public boolean addMyarticle(MyarticleModel model) {
         String sql = "insert into t_myarticle(uname,theme,content,likenum,collection,classify,postdate) values (?,?,?,?,?,?,?);";
 
 //        System.out.println("insert into t_myarticle(uname,theme,content,likenum,collection,classify,postdate) values (\""+model.getUname()+"\",\""+model.getTheme()+"\",\""+model.getContent()+"\",\""+model.getLikenum()+"\","+model.getCollection()+","+model.getClassify().getName()+",\""+model.getPostdate()+"\");");
-        try{
-            int num =  jdbc_link.update(sql,model.getUname(),model.getTheme(),model.getContent(),model.getLikenum(),model.getCollection(),model.getClassify().getName(),model.getPostdate());
-            if(num != 0){
+        try {
+            int num = jdbc_link.update(sql, model.getUname(), model.getTheme(), model.getContent(), model.getLikenum(), model.getCollection(), model.getClassify().getName(), model.getPostdate());
+            if (num != 0) {
                 return true;
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -161,16 +159,14 @@ public class loginServiceDao implements IServiceDao {
     }
 
 
-
-
     //修改点赞数和收藏数
     @Override
-    public Boolean updateNumColl(MyarticleModel model){
+    public Boolean updateNumColl(MyarticleModel model) {
         try {
             String sql = "UPDATE `t_myarticle` SET likenum = ?, collection =? WHERE id = ?;";
             BeanPropertyRowMapper<MyarticleModel> mapper = new BeanPropertyRowMapper<>(MyarticleModel.class);
-            jdbc_link.update(sql,mapper,model.getLikenum(),model.getCollection(),model.getId());
-        }catch (Exception e){
+            jdbc_link.update(sql, mapper, model.getLikenum(), model.getCollection(), model.getId());
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
@@ -178,6 +174,16 @@ public class loginServiceDao implements IServiceDao {
         return true;
         //////
     }
+
+
+    @Override
+    public InfoModel getInfoModel(LoginModel loginModel) {
+        String sql = "select id, phone ,email ,headimg, fins  from `t_info` where `uname` = ?";
+        BeanPropertyRowMapper<InfoModel> info = new BeanPropertyRowMapper<>(InfoModel.class);
+        return jdbc_link.queryForObject(sql, info, loginModel.getName());
+    }
+}
+
 //    public boolean password(Login login){  //查询站好密码
 //        //用来遍历数据库所有的   where 指定的
 //        String sql="SELECT * FROM `cheshibiao`";
@@ -200,15 +206,3 @@ public class loginServiceDao implements IServiceDao {
 //        return false;
 //    }
 
-    @Override
-    public InfoModel getInfoModel(LoginModel loginModel){
-        String sql = "select id, phone ,email ,headimg, fins  from `t_info` where `uname` = ?";
-        BeanPropertyRowMapper<InfoModel> info = new BeanPropertyRowMapper<>();
-        return  jdbc_link.queryForObject(sql,info,loginModel.getName());
-    }
-@Test
-public void abc(){
-    System.out.println("11");
-}
-
-}
