1f754a2810c70051446afcc8e18494e3d4985555
your_username <911412667@qq.com>
2022-12-04 20:08:39 +0800

    大bug解决完成3

---
+++ src/main/java/coma/Web/Personal_dataAction.java
@@ -0,0 +1,26 @@
+package coma.Web;
+
+import coma.Form.Father;
+import coma.Form.Pzwj;
+import coma.Iservice.IServiceDao;
+import coma.Service.Main01Filter;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Component("Personal_dataAction")
+public class Personal_dataAction extends Action{
+    @Resource
+    IServiceDao dao; //数据库
+
+    @Override
+    public void execute(Father father, Pzwj pzwj, HttpServletRequest req, HttpServletResponse resp, Main01Filter main) throws ServletException, IOException {
+
+        System.out.println("---------------进入个人资料界面-------------------");
+        main.processTemplate(pzwj.getLiu(),req,resp); //跳转网页
+    }
+
+}
