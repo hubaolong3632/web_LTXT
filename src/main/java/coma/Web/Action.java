a0fe36cb2ddcce3eda6a0fc93331caf81d6429c3
your_username <911412667@qq.com>
2022-12-05 18:45:44 +0800

    大bug解决完成3

--- src/main/java/coma/Web/Action.java
+++ src/main/java/coma/Web/Action.java
@@ -2,19 +2,18 @@ package coma.Web;
 
 import coma.Form.Father;
 import coma.Form.Pzwj;
-import coma.Iservice.IServiceDao;
 import coma.Service.Main01Filter;
 
-import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-/***
- * 通过子类实现此方法用来进行spring操作
- */
+///***
+// * 通过子类实现此方法用来进行spring操作
+// */
 
 public abstract class Action {
+
 
     public abstract void execute(Father father, Pzwj pzwj, HttpServletRequest req, HttpServletResponse resp, Main01Filter main) throws ServletException, IOException;
 }
